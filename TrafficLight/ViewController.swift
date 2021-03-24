4caff35b512dcdf344ca2a3e3e3e05b59189b484
Oksana Tugusheva <gutuxa@gmail.com>
2021-03-24 07:54:01 +0300

    HW_2.1.2 Main behaviour

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    var lights: [UIView] = []
+    var isStartButtonPressed = false
+    var nextActiveLightIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lights = [redLightView, yellowLightView, greenLightView]
+        
+        for light in lights {
+            light.layer.cornerRadius = 50
+            light.alpha = 0.3
+        }
+        
+        startButton.layer.cornerRadius = 5
     }
 
-
+    @IBAction func startButtonPressed() {
+        if !isStartButtonPressed {
+            isStartButtonPressed = true
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        for (lightIndex, light) in lights.enumerated() {
+            if lightIndex == nextActiveLightIndex {
+                light.alpha = 1
+            } else {
+                light.alpha = 0.3
+            }
+        }
+        
+        nextActiveLightIndex += 1
+        
+        if nextActiveLightIndex == lights.count {
+            nextActiveLightIndex = 0
+        }
+    }
 }
 
